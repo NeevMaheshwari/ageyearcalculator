40dc39985ae35c08c9c688c1232296c03f6569a4
Neev Maheshwari <neeviscool5@gmail.com>
2023-06-19 23:40:47 +0530

    Add files via upload

---
+++ name and age year.py
@@ -0,0 +1,8 @@
+name=str(input("Enter your name: "))
+age=eval(input("Enter your age: "))
+if age>=100:
+    print("You are already 100 years old")
+else:
+ y=100-age
+ year=2023+y
+ print(name,"you will be a hundred years old in",year)
